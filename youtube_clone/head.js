1e64f4ba9208c56ea19cc432487b4e7fcafe4c1a
kelvinquarcoo <117761564+kelvinquarcoo@users.noreply.github.com>
2023-03-28 01:55:59 +0000

    Add files via upload

---
+++ youtube_clone/head.js
@@ -0,0 +1,15 @@
+/*console.log('het');
+var first = 'Ada';
+nam = first[first.length -1];
+console.log(nam);
+
+function phrase(noun, adjective, verb, adverb) {
+    var result = '';
+    result += 'The ' + adjective + noun + verb + adverb;
+    return result;
+}
+
+console.log(phrase('dog', 'big', 'ran', 'very fast'));*/
+
+var array = [['John', 23], ['heya', 45]];
+[[][]]
